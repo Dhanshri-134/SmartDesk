fe21c9f451de4443466f149e95b06d73947a2605
Dhanshri-134 <yewankardhanshri@gmail.com>
2025-07-27 23:29:49 +0530

    Admin Dashboard in Web Portal is Done - DY

--- web-portal/compWeb/members/urls.py
+++ web-portal/compWeb/members/urls.py
@@ -1,13 +1,19 @@
+from django.conf import settings
 from django.urls import path
 from . import views
 
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('members/', views.members, name='members'),
+    path('', views.members, name='members'),
     path('index/', views.index, name='index'),
-    path('blog/', views.blog, name='blog'),
     path('contact_form/', views.contactForm, name='contact_form'),
     path('newsletter_signup/', views.index, name='newsletter_signup'),
-    path('home/', views.index, name='home'),
+    path('adminLogin/', views.admin_login, name='admin_login'),
+    path('adminDashboard/', views.dashboard, name='adminLogin'),
+    path('hiredEmp/', views.hiredEmp, name='hiredEmp'),
     path('offer-letter/', views.generate_offer_letter, name='offer_letter'),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web-portal/compWeb/members/views.py
+++ web-portal/compWeb/members/views.py
@@ -1,12 +1,19 @@
 import datetime
 import os
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.template.loader import render_to_string,get_template
 from xhtml2pdf import pisa
 from django.templatetags.static import static
 import io
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from members.models import Applicant
+
+
 
 def members(request):
   template = loader.get_template('index.html')
@@ -26,17 +33,25 @@ def serviceDetails(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
 
-def StarterPage(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
-
-def blogDetails(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+def hiredEmp(request):
+    applicants = Applicant.objects.all()
+    return render(request, 'hiredEmp.html', {'applicants': applicants})
 
 def contactForm(request):
   template = loader.get_template('index.html')
   return HttpResponse(template.render())
+
+def dashboard(request):
+    applicants = Applicant.objects.all()
+    return render(request, 'adminDashboard.html', {'applicants': applicants})
+
+def update_status(request, id):
+    if request.method == "POST":
+        applicant = get_object_or_404(Applicant, id=id)
+        new_status = request.POST.get('status')
+        applicant.status = new_status
+        applicant.save()
+        return JsonResponse({'success': True})
 
 
 def generate_offer_letter(request):
@@ -66,3 +81,16 @@ def generate_offer_letter(request):
         return HttpResponse(result.getvalue(), content_type='application/pdf')
 
     return HttpResponse("Error while generating PDF", status=500)
+
+
+def admin_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None and user.is_staff: 
+            login(request, user)
+            return redirect('adminDashboard')  #
+        else:
+            messages.error(request, 'Invalid credentials or not an admin.')
+    return render(request, 'admin_login.html')
